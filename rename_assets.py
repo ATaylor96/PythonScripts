70d14f4a40045dc083959b1ba3a84547e7fe121f
AdamTaylor <adam.f.taylor@hotmail.com>
2022-10-31 21:36:35 +0000

    Update the search and replace script with a Qt GUI

--- rename_assets.py
+++ rename_assets.py
@@ -1,5 +1,8 @@
 import unreal
+import sys
+sys.path.append("F:/Projects/Repo/Python/Unreal/PythonScripts/venv/Lib/site-packages")
 
+from PySide2 import QtCore, QtGui, QtUiTools, QtWidgets
 
 def rename_assets(search_pattern, replace_pattern, use_case):
     # instances of unreal classes
@@ -33,4 +36,41 @@ def rename_assets(search_pattern, replace_pattern, use_case):
     unreal.log("Replaced {} of {} assets".format(replaced, num_assets))
 
 
-rename_assets("Old", "New", False)
+class RenameGUI(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(RenameGUI, self).__init__(parent)
+
+        # load the created ui widget
+        self.widget = QtUiTools.QUiLoader().load("F:\\Projects\\Repo\\Python\\Unreal\\PythonScripts\\Qt\\search_and_replace.ui")
+
+        # attach the widget to the "self" GUI
+        self.widget.setParent(self)
+
+        # set the UI geometry (if UI is not centered/visible)
+        self.widget.setGeometry(0, 0, self.widget.width(), self.widget.height())
+
+        # find the elements
+        self.find = self.widget.findChild(QtWidgets.QLineEdit, "findLineEdit")
+        self.replace = self.widget.findChild(QtWidgets.QLineEdit, "replaceLineEdit")
+        self.match_case = self.widget.findChild(QtWidgets.QCheckBox, "matchCaseCheckBox")
+
+        # assign trigger to button
+        self.rename_button = self.widget.findChild(QtWidgets.QPushButton, "replaceAllPushButton")
+        self.rename_button.clicked.connect(self.rename_handler)
+
+    def rename_handler(self):
+        search_pattern = self.find.text()
+        replace_pattern = self.replace.text()
+        use_case = self.match_case.isChecked()
+
+        rename_assets(search_pattern, replace_pattern, use_case)
+
+
+# only create an instance when it is not already running
+app = None
+if not QtWidgets.QApplication.instance():
+    app = QtWidgets.QApplication(sys.argv)
+
+main_window = RenameGUI()
+main_window.show()
+
